0e595d45e90c8aea836c219704b318993f72a37f
Jeannette Tang <jeannette.tang@utexas.edu>
2016-04-07 16:41:36 -0500

    dynamic pages

--- python-web-app/app/app.py
+++ python-web-app/app/app.py
@@ -7,12 +7,18 @@ from make_db import (create_adoptable, create_breed, create_Breeds, create_Adopt
 from models import db, Adoptable, AdoptableBreed, Breed, BreedOrganization, Organization, AdoptableImage, BreedImage
 import requests
 import subprocess
+import json
+import simplejson
+import urllib
+import os
+os.environ['no_proxy'] = '127.0.0.1, localhost'
 
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 # es = ElasticSearch('http://23.253.111.129:9200/')
 
+# REAL THING
 
 @app.route('/', methods=['GET','POST'])
 @app.route('/index')
@@ -20,68 +26,62 @@ def index():
 	return render_template("index.html")
 
 @app.route('/breeds')
-def breeds():
-	return render_template("breeds.html")
+@app.route('/breeds/<breed>')
+def breeds(breed=None):
 
-@app.route('/testAdoptables')
-def testAdoptable():
-	results = create_Adoptables()
-	return jsonify(results)
+    if breed is not None:
+    	url = "http://catsgalore.me/api/breeds/" + breed
+    	response = urllib.urlopen(url)
+    	data1 = json.loads(response.read())
+    	url = "http://catsgalore.me/api/breeds/" + breed
+    	response = urllib.urlopen(url)
+    	data1 = json.loads(response.read())
+        return render_template('models/breed.html', breed=data['breed'] )
+    else:
+    	breeds = breeds_api()
 
-@app.route('/testDB')
-def testDB():
-	x = create_Breeds()
-	y = create_Adoptables()
-	results = {"Breeds Created": x, "Adoptables Creaded": y}
-	return jsonify(results)
+    	url = "http://catsgalore.me/api/breeds/" + "?page_size=10000"
+    	response = urllib.urlopen(url)
+    	data = json.loads(response.read())
+    return render_template('breeds.html', breeds=data['breeds'])
 
 @app.route('/adoptables')
-def adoptables():
-	return render_template("adoptables.html")
+@app.route('/adoptables/<adoptable>')
+def adoptables(adoptable=None):
+
+    if adoptable is not None:
+        url = "http://catsgalore.me/api/adoptables/" + adoptable
+    	response = urllib.urlopen(url)
+    	data = json.loads(response.read())
+        return render_template('models/adoptable.html', adoptable=data['adoptable'] )
+    else:
+        adoptables = adoptables_api()
+
+        url = "http://catsgalore.me/api/adoptables/" + + "?page_size=10000"
+        response = urllib.urlopen(url)
+        data = json.loads(response.read())
+    return render_template('adoptables.html', adoptables=data['adoptables'])
 
 @app.route('/organizations')
-def organizations():
-	return render_template("organizations.html")
+@app.route('/organizations/<organization>')
+def organizations(organization=None):
+
+    if organization is not None:
+        url = "http://catsgalore.me/api/organizations/" + organization
+    	response = urllib.urlopen(url)
+    	data = json.loads(response.read())
+        return render_template('models/organization.html', organization=data['organization'] )
+    else:
+    	organizations = Organization.query.all()
+
+    	url = "http://catsgalore.me/api/organizations/" + "?page_size=10000"
+    	response = urllib.urlopen(url)
+    	data = json.loads(response.read())
+    return render_template('organizations.html', organizations=data['organizations'])
 
 @app.route('/about')
 def about():
 	return render_template("about.html")
-
-@app.route('/breeds/1')
-def breeds_1():
-	return render_template("models/breeds/1.html")
-
-@app.route('/breeds/2')
-def breeds_2():
-	return render_template("models/breeds/2.html")
-
-@app.route('/breeds/3')
-def breeds_3():
-	return render_template("models/breeds/3.html")
-
-@app.route('/adoptables/1')
-def adoptables_1():
-	return render_template("models/adoptables/1.html")
-
-@app.route('/adoptables/2')
-def adoptables_2():
-	return render_template("models/adoptables/2.html")
-
-@app.route('/adoptables/3')
-def adoptables_3():
-	return render_template("models/adoptables/3.html")
-
-@app.route('/organizations/1')
-def organizations_1():
-	return render_template("models/organizations/1.html")
-
-@app.route('/organizations/2')
-def organizations_2():
-	return render_template("models/organizations/2.html")
-
-@app.route('/organizations/3')
-def organizations_3():
-	return render_template("models/organizations/3.html")
 
 @app.route('/organizations/error')
 def organizations_error():
@@ -94,6 +94,18 @@ def breeds_error():
 @app.route('/adoptables/error')
 def adoptables_error():
 	return render_template("errors/error_noAdoptable.html")
+
+@app.route('/testAdoptables')
+def testAdoptable():
+	results = create_Adoptables()
+	return jsonify(results)
+
+@app.route('/testDB')
+def testDB():
+	x = create_Breeds()
+	y = create_Adoptables()
+	results = {"Breeds Created": x, "Adoptables Creaded": y}
+	return jsonify(results)
 
 # API
 
@@ -132,7 +144,6 @@ def breed_api(breed_id):
 
 
 
-
 # ADOPTABLES
 @app.route('/api/adoptables/', methods = ['GET'])
 def adoptables_api():
@@ -143,7 +154,7 @@ def adoptables_api():
 	else:
 		page = int(page)
 	if page_size is None:
-		page_size = 5
+		page_size = 1000
 	else:
 		page_size = int(page_size)
 	beginning = page*page_size
@@ -199,7 +210,6 @@ def adoptablesByOrganization_api(organization_id):
 	adoptables = adoptables[beginning : end]
 	adoptables = update_adoptables(convert_to_json(adoptables))
 	return jsonify(adoptables=adoptables)
-
 
 
 
@@ -280,3 +290,225 @@ def update_adoptables(adoptables):
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
+
+# END OF REAL THING
+
+
+# FAKE THING
+
+# @app.route('/', methods=['GET','POST'])
+# @app.route('/index')
+# def index():
+# 	return render_template("index.html")
+
+# @app.route('/breeds')
+# @app.route('/breeds/<breed>')
+# def breeds(breed=None):
+
+#     if breed is not None:
+#         return render_template('models/breed.html', breed=breed_api(breed), adoptables=adoptablesByBreed_api(breed) )
+#     else:
+#     	breeds = breeds_api()
+#     return render_template('breeds.html', breeds=breeds)
+
+# @app.route('/adoptables')
+# @app.route('/adoptables/<adoptable>')
+# def adoptables(adoptable=None):
+
+#     if adoptable is not None:
+#         return render_template('models/adoptable.html', adoptable=adoptable_api(adoptable) )
+#     else:
+#         adoptables = adoptables_api()
+#     return render_template('adoptables.html', adoptables=adoptables)
+
+# @app.route('/organizations')
+# @app.route('/organizations/<organization>')
+# def organizations(organization=None):
+
+#     if organization is not None:
+#         return render_template('models/organization.html', organization=organization_api(organization), adoptables=adoptablesByOrganization_api(organization) )
+#     else:
+#     	organizations = organizations_api()
+#     return render_template('organizations.html', organizations=organizations)
+
+# @app.route('/about')
+# def about():
+# 	return render_template("about.html")
+
+# @app.route('/organizations/error')
+# def organizations_error():
+# 	return render_template("errors/error_noOrganization.html")
+
+# @app.route('/breeds/error')
+# def breeds_error():
+# 	return render_template("errors/error_noBreed.html")
+
+# @app.route('/adoptables/error')
+# def adoptables_error():
+# 	return render_template("errors/error_noAdoptable.html")
+
+# @app.route('/testAdoptables')
+# def testAdoptable():
+# 	results = create_Adoptables()
+# 	return jsonify(results)
+
+# @app.route('/testDB')
+# def testDB():
+# 	x = create_Breeds()
+# 	y = create_Adoptables()
+# 	results = {"Breeds Created": x, "Adoptables Creaded": y}
+# 	return jsonify(results)
+
+
+# # API
+# # BREEDS
+# @app.route('/api/breeds/', methods = ['GET'])
+# def breeds_api():
+# 	breeds = Breed.query.all()
+# 	breeds = update_breeds(convert_to_json(breeds))
+# 	return breeds
+
+# @app.route('/api/breeds/<int:breed_id>', methods = ['GET'])
+# def breed_api(breed_id):
+# 	breed = update_breed((Breed.query.get(breed_id)).to_json())
+# 	return breed
+
+
+
+# # ADOPTABLES
+# @app.route('/api/adoptables/', methods = ['GET'])
+# def adoptables_api():
+# 	adoptables = Adoptable.query.all()
+# 	adoptables = update_adoptables(convert_to_json(adoptables))
+# 	return adoptables
+
+# @app.route('/api/adoptables/<int:adoptable_id>', methods = ['GET'])
+# def adoptable_api(adoptable_id):
+# 	adoptable = update_adoptable((Adoptable.query.get(adoptable_id)).to_json())
+# 	return adoptable
+
+# @app.route('/api/adoptables/breed/<int:breed_id>', methods = ['GET'])
+# def adoptablesByBreed_api(breed_id):
+# 	page = request.args.get('page')
+# 	page_size = request.args.get('page_size')
+# 	if page is None:
+# 		page = 0
+# 	else:
+# 		page = int(page)
+# 	if page_size is None:
+# 		page_size = 4
+# 	else:
+# 		page_size = int(page_size)
+
+# 	allAdoptables = Adoptable.query.all()
+# 	adoptables = []
+# 	for adoptable in allAdoptables:
+# 		breeds = AdoptableBreed.query.filter(AdoptableBreed.adoptable_id==adoptable.id)
+# 		for breed in breeds:
+# 			if breed.breed_id == breed_id:
+# 				adoptables.append(adoptable)
+# 				break
+# 	beginning = page*page_size
+# 	end = (page*page_size) + page_size
+
+# 	adoptables = adoptables[beginning : end]
+# 	adoptables = update_adoptables(convert_to_json(adoptables))
+# 	return adoptables
+
+
+# @app.route('/api/adoptables/organization/<int:organization_id>', methods = ['GET'])
+# def adoptablesByOrganization_api(organization_id):
+# 	page = request.args.get('page')
+# 	page_size = request.args.get('page_size')
+# 	if page is None:
+# 		page = 0
+# 	else:
+# 		page = int(page)
+# 	if page_size is None:
+# 		page_size = 4
+# 	else:
+# 		page_size = int(page_size)
+
+# 	allAdoptables = Adoptable.query.all()
+# 	adoptables = []
+# 	for adoptable in allAdoptables:
+# 		if adoptable.org_id == organization_id:
+# 			adoptables.append(adoptable)
+
+# 	beginning = page*page_size
+# 	end = (page*page_size) + page_size
+
+# 	adoptables = adoptables[beginning : end]
+# 	adoptables = update_adoptables(convert_to_json(adoptables))
+# 	return adoptables
+
+
+
+# # ORGANIZATIONS
+# @app.route('/api/organizations/', methods = ['GET'])
+# def organizations_api():
+# 	organizations = Organization.query.all()
+# 	organizations = convert_to_json(organizations)
+# 	return organizations
+
+# @app.route('/api/organizations/<int:organization_id>', methods = ['GET'])
+# def organization_api(organization_id):
+# 	organization = Organization.query.get(organization_id).to_json()
+# 	return organization
+
+
+# # Helper functions
+# def convert_to_json(listToConvert):
+# 	convertedList = []
+# 	for item in listToConvert:
+# 		convertedList.append(item.to_json())
+# 	return convertedList
+
+
+# def update_breed(breed):
+# 	image_info = BreedImage.query.filter(BreedImage.breed_id==breed['id'])
+# 	images = []
+# 	for i in image_info:
+# 		images.append(i.url)
+# 	breed['images'] = images
+
+# 	return breed
+
+# def update_breeds(breeds):
+# 	updatedBreeds = []
+# 	for breed in breeds:
+# 		updatedBreeds.append(update_breed(breed))
+# 	return updatedBreeds
+
+
+
+# def update_adoptable(adoptable):
+# 	breeds = []
+# 	breed_ids = []
+# 	breed_info = AdoptableBreed.query.filter(AdoptableBreed.adoptable_id==adoptable['id'])
+# 	for b in breed_info:
+# 		breed_ids.append(b.breed_id)
+# 		breeds.append(Breed.query.filter(Breed.id==b.breed_id).first().name)
+# 	adoptable['breed_ids'] = breed_ids
+# 	adoptable['breeds'] = breeds
+
+# 	image_info = AdoptableImage.query.filter(AdoptableImage.adoptable_id==adoptable['id'])
+# 	images = []
+# 	for i in image_info:
+# 		images.append(i.url)
+# 	adoptable['images'] = images
+
+# 	return adoptable
+
+# def update_adoptables(adoptables):
+# 	updatedAdoptables = []
+# 	for adoptable in adoptables:
+# 		updatedAdoptables.append(update_adoptable(adoptable))
+# 	return updatedAdoptables
+
+
+# if __name__ == '__main__':
+# 	app.debug = True
+# 	app.run()
+
+# END OF FAKE THING
